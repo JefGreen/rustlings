c837aea6ee9b0ac4a5f9ed10ed63f7dc82223bde
Jeff Trempe <trempe.jeff@gmail.com>
2023-11-26 23:29:41 -0500

    redoing string module

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,6 +3,8 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
+// I AM NOT DONE
+
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
     let answer = input.trim();
