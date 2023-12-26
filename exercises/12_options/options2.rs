49614bd42a8d6d1774a5abf841581595943fdf9a
Jeff Trempe <trempe.jeff@gmail.com>
2023-12-26 13:48:08 -0500

    errors1 simple option implementation

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -11,7 +11,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        if let word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, Some(target));
         }
     }
